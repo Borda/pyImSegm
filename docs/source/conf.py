4c2be23182b65b131cb83be6cdb4c605269947cf
J. Borovec <jirka.borovec@seznam.cz>
2020-04-06 09:58:00 +0200

    sphinx versions

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -60,7 +60,7 @@ release = imsegm.__version__
 
 # If your documentation needs a minimal Sphinx version, state it here.
 
-needs_sphinx = '1.4'
+needs_sphinx = '2.2'
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
