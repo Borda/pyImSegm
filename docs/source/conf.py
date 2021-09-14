622a2dc142c71da2a20f816a8ef19feff7814592
Jirka <jirka.borovec@seznam.cz>
2021-09-14 23:06:19 +0200

    docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -75,7 +75,7 @@ with open('readme.md', 'w') as fp:
 
 # If your documentation needs a minimal Sphinx version, state it here.
 
-needs_sphinx = '2.4'
+needs_sphinx = '4.2'
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
